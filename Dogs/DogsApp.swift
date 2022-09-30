7e9c180f6d4e3aa4809966f6780a7f19ded331a3
DamianoPellegrini <damiano1.pellegrini@gmail.com>
2022-09-30 16:37:40 +0200

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Damiano Pellegrini on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
